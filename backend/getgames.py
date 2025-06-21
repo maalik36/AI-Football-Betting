6932600e3fd7e00a539ac4e83daaf1d452e2cd82
boxviet <156624375+boxviet@users.noreply.github.com>
2025-06-21 14:31:50 -0700

    Update getgames.py
    Made it fetch image urls

--- backend/getgames.py
+++ backend/getgames.py
@@ -5,7 +5,7 @@ def getnextgames(key):
     client = genai.Client(api_key=key)
 
 
-    csv_path = "backend\\games.csv"
+    csv_path = "backend\games.csv"
     with open(csv_path, "r") as f:
         csv_data = f.read()
 
@@ -18,6 +18,7 @@ def getnextgames(key):
     {csv_data_snippet}
 
     Please output the next 5 games, based on the current date, and assuming that all these games are in the year 2025. Do not output anything extra. Just the games, nothing else. Make the output a json file.
+    Include image urls of both of the teams, store both of them in the json that will be outputted.
     """
 
     response = client.models.generate_content(
